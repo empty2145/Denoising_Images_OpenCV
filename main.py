d3d2a96a48d9b89277d063077f1af068b3f8b619
empty2145 <tasybaevtamir@gmail.com>
2026-04-17 22:56:27 +0500

    finalised the model

--- main.py
+++ main.py
@@ -4,5 +4,42 @@ import matplotlib.pyplot as plt
 
 
 def denoise_image(image_path):
-    noisy_image = cv2.imread(imaage_path)
-    noisy_image = cv2.cvtColor(noisy_image, cv2.COLOR_BGR2RGB)
+    noisy_image = cv2.imread(image_path)
+    noisy_image_rgb = cv2.cvtColor(noisy_image, cv2.COLOR_BGR2RGB)
+
+    gaussian_denoised_img = cv2.GaussianBlur(noisy_image, (5, 5), 0)
+    gaussian_denoised_img_rgb = cv2.cvtColor(gaussian_denoised_img, cv2.COLOR_RGB2BGR)
+
+    median_denoised_img = cv2.medianBlur(noisy_image, 5)
+    median_denoised_img_rgb = cv2.cvtColor(median_denoised_img, cv2.COLOR_RGB2BGR)
+
+    bilateral_denoised_img = cv2.bilateralFilter(noisy_image, 9, 75, 75)
+    bilateral_denoised_img_rgb = cv2.cvtColor(bilateral_denoised_img, cv2.COLOR_RGB2BGR)
+
+    plt.figure(figsize=(15, 10))
+    plt.subplot(2, 2, 1)
+    plt.title('Noisy Image')
+    plt.imshow(noisy_image_rgb)
+    plt.axis('off')
+
+    plt.subplot(2, 2, 2)
+    plt.title('Gaussian Blur Image')
+    plt.imshow(gaussian_denoised_img_rgb)
+    plt.axis('off')
+
+    plt.subplot(2, 2, 3)
+    plt.title('Median Blur Image')
+    plt.imshow(median_denoised_img_rgb)
+    plt.axis('off')
+
+    plt.subplot(2, 2, 4)
+    plt.title('Bilateral filter Image')
+    plt.imshow(bilateral_denoised_img_rgb)
+    plt.axis('off')
+
+    plt.show()
+
+
+denoise_image('noisy1.png')
+denoise_image('noisy2.png')
+denoise_image('noisy3.jpeg')
